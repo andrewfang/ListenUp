578e87e3c8adb1ebd247e278c68e628c9ccd91c9
Anju <anju.angelica@gmail.com>
2014-04-04 21:53:09 -0700

    Working audio without errors

--- ListenUp!/gen/edu/berkeley/cs160/jaguars/listenup/R.java
+++ ListenUp!/gen/edu/berkeley/cs160/jaguars/listenup/R.java
@@ -18,8 +18,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int app_icon=0x7f020000;
@@ -28,19 +28,18 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int button_background=0x7f080004;
-        public static final int checkBox1=0x7f080006;
-        public static final int checkBox2=0x7f080007;
-        public static final int checkBox3=0x7f080008;
-        public static final int imageView1=0x7f080000;
-        public static final int maxAmpBar=0x7f080002;
-        public static final int seekBar1=0x7f08000b;
-        public static final int startStop=0x7f080003;
-        public static final int textView1=0x7f08000a;
-        public static final int textView3=0x7f080001;
-        public static final int textView5=0x7f080009;
-        public static final int textView7=0x7f080005;
+        public static final int action_settings=0x7f09000b;
+        public static final int button_background=0x7f090004;
+        public static final int checkBox1=0x7f090006;
+        public static final int checkBox2=0x7f090007;
+        public static final int checkBox3=0x7f090008;
+        public static final int imageView1=0x7f090000;
+        public static final int maxAmpBar=0x7f090002;
+        public static final int seekBar1=0x7f09000a;
+        public static final int startStop=0x7f090003;
+        public static final int textView1=0x7f090009;
+        public static final int textView3=0x7f090001;
+        public static final int textView7=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -50,15 +49,18 @@ public final class R {
         public static final int settings=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+    }
+    public static final class raw {
+        public static final int carhonk1=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int backgroundRun=0x7f050005;
-        public static final int hello_world=0x7f050001;
-        public static final int start=0x7f050003;
-        public static final int stop=0x7f050004;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int backgroundRun=0x7f060005;
+        public static final int hello_world=0x7f060001;
+        public static final int start=0x7f060003;
+        public static final int stop=0x7f060004;
     }
     public static final class style {
         /** 
@@ -82,10 +84,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
